2d16785bb37cec67b366917f572ea47007bebea2
Mattia Sestili <mattia.sestili@gmail.com>
2016-08-04 16:50:45 +0100

    Shooting Code

--- Space Shooter/Assets/Script/PlayerController.cs
+++ Space Shooter/Assets/Script/PlayerController.cs
@@ -1,9 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Boundary boundary;
+    public float tilt;
+    public float fireRate;
+    private float nextFire;
+
+    public GameObject shot;
+    public Transform shotSpawn; //shotSpawn.transform.position
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -11,8 +34,15 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Rigidbody rig = GetComponent<Rigidbody>();
-        rig.velocity = movement * speed;
+        GetComponent<Rigidbody>().velocity = movement * speed;
+        GetComponent<Rigidbody>().position = new Vector3
+            (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
     }
 }
  
